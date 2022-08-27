89ddf1c346d4ce81d06865f78e0b56c2edf53a83
Diego Lopez <ndzerglink@live.jp>
2022-08-27 23:39:41 +0900

    Added screenshot for fmLaPaz plugin

--- notif_plugin/README.md
+++ notif_plugin/README.md
@@ -1,8 +1,8 @@
-## MacOS version
+# MacOS version
 
 ![Notification](now_fmLaPaz.png)
 
-#installation process:
+##installation process:
 
 Download $this folder as a zip file and expand it to $HOME/Public folder
 
@@ -16,10 +16,17 @@ The application will run every 4 minutes and will display
 
 the current song playing on https://www.lapaz.fm in the Notification Center.
 
+## xbar plugin
+Download [xbar app](https://github.com/matryer/xbar-plugins) and add [this file](https://raw.githubusercontent.com/ndlopez/fmLaPazNow/main/notif_plugin/getFmLaPaz.4m.sh) to your xbar/plugins folder.
+
+![plugin](fmLaPaz_plugin_prev.png)
+
+As seen on the screenshot not only the current song is displayed but also nice artwork.
+ 
 -------
 
 Programming language: JavaScript, Bash
 
-Editor: VS Code
+Editor: Emacs/VIM
 
 Environment: MacBookPro/ MacOS 15.5
---
+++ notif_plugin/getFmLaPaz.4m.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# <xbar.title>NowPlaying</xbar.title>
+# <xbar.version>0.2</xbar.version>
+# <xbar.author>Diego Lopez</xbar.author>
+# <xbar.author.github>ndlopez</xbar.author.github>
+# <xbar.desc>Fetch data from lapaz.fm and scrapes current song and artwork</xbar.desc>
+# <xbar.image>https://github.com/ndlopez/weather_app/raw/notif/notif_app/currWeather_1h_prev.png</xbar.image>
+# <xbar.dependencies>bash,curl,tr,cut</xbar.dependencies>
+# <xbar.abouturl>https://github.com/ndlopez/fmLaPazNow/notif_plugin/</xbar.abouturl>
+
+url=https://stream.consultoradas.com/cp/get_info.php?p=8042
+
+gotData=`/usr/bin/curl -k --silent $url`
+#gotData=`cat fmLaPaz_now.json`
+
+if [ ! $? == 0 ];then
+    echo "URL error"
+    echo "---"
+    echo "Cannot access lapaz.fm"
+fi
+
+currSong=$(echo $gotData | cut -f4 -d'"')
+currArtwork=$(echo $gotData | cut -f8 -d'"' | sed 's%\\%%g')
+getArtwork=`curl --silent ${currArtwork} | base64`
+
+echo $currSong
+echo "---"
+#echo ${currArtwork//\\/}
+echo "| image=${getArtwork}"
+echo "Now playing on FM La Paz"
