33d418aae3a4357964634c21a9b718a9eed8bc2a
Diego Lopez <ndzerglink@live.jp>
2022-08-29 22:56:37 +0900

    Improved disp_data HTML version

--- html_display/get_fmLaPaz.js
+++ html_display/get_fmLaPaz.js
@@ -1,12 +1,23 @@
 //Should fetch data from fm La Paz
-fetch('https://icecasthd.net:2199/rpc/lapazfm/streaminfo.get')
-.then((response) => {
-	return response.json()
-})
-.then((json_data)=>{
-console.log(json_data['data'][0]['song'])
-})
-.catch((err)=>{
-	console.log("Couldnt fetch json_data")
-})
+const third_rock = "https://feed.tunein.com/profiles/s151799/nowPlaying";
+const fmLaPaz = "https://stream.consultoradas.com/cp/get_info.php?p=8042";
+
+display_data();
+
+async function display_data(){
+    const key = "title";
+    const gotData = await get_url(fmLaPaz);
+    const img_art = "<img src='"+gotData.artwork+"' alt='Now Playing'>";
+    document.getElementById("currSong").innerHTML = gotData.song;
+    document.getElementById("currArt").innerHTML = img_art;
+    console.log("Now: "+ gotData.song,gotData.artwork);
+}
+
+async function get_url(my_url){
+    const response = await fetch(my_url);
+    const data = await response.json();
+    const song = data['title'];
+    const artwork = data['art'];
+    return {song,artwork};
+}
 
