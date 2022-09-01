40b593fad7c0486a563b2812094f443eb28a49a9
Diego Lopez <ndzerglink@live.jp>
2022-09-01 21:18:17 +0900

    Updated HTML page

--- html_display/get_fmLaPaz.js
+++ html_display/get_fmLaPaz.js
@@ -3,6 +3,9 @@ const third_rock = "https://feed.tunein.com/profiles/s151799/nowPlaying";
 const fmLaPaz = "https://stream.consultoradas.com/cp/get_info.php?p=8042";
 
 let origTitle = document.title;
+var timeNow = new Date();
+let hh = timeNow.getHours();
+let mm = timeNow.getMinutes();
 
 //change title when focus
 function oldTitle(){
@@ -22,9 +25,21 @@ async function display_data(){
     const key = "title";
     const gotData = await get_url(fmLaPaz);
     const img_art = "<img src='"+gotData.artwork+"' alt='Now Playing' width=350>";
-    document.getElementById("currSong").innerHTML = gotData.song;
-    document.getElementById("currArt").innerHTML = img_art;
     document.title = gotData.song;
+    
+    //document.getElementById("currSong").innerHTML = gotData.song;
+    //document.getElementById("currArt").innerHTML = img_art;
+    const divElm = document.createElement("div");
+    const h2Title = document.createElement("h2");
+    h2Title.innerHTML = hh + ":" + mm + " Now Playing on FM La Paz";
+    const h2Song = document.createElement("h2");
+    h2Song.innerHTML = gotData.song;
+    const divImg = document.createElement("div");
+    divImg.innerHTML = img_art;
+    divElm.appendChild(h2Title);
+    divElm.appendChild(h2Song);
+    divElm.appendChild(divImg);
+    document.body.appendChild(divElm);
     //console.log("Now: "+ gotData.song,gotData.artwork);
 }
 
