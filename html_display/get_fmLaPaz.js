4f4170beb925aede85fa972736e459c25d71ffb5
Diego Lopez <ndzerglink@live.jp>
2022-09-01 20:45:00 +0900

    Updated page title

--- html_display/get_fmLaPaz.js
+++ html_display/get_fmLaPaz.js
@@ -2,7 +2,21 @@
 const third_rock = "https://feed.tunein.com/profiles/s151799/nowPlaying";
 const fmLaPaz = "https://stream.consultoradas.com/cp/get_info.php?p=8042";
 
+let origTitle = document.title;
+
+//change title when focus
+function oldTitle(){
+    document.title = origTitle;
+}
+function newTitle(thisTitle){
+    document.title = thisTitle;
+}
+
+//window.onfocus = newTitle(display_data());
+//window.onfocus = oldTitle();
+
 display_data();
+
 
 async function display_data(){
     const key = "title";
@@ -10,7 +24,8 @@ async function display_data(){
     const img_art = "<img src='"+gotData.artwork+"' alt='Now Playing' width=350>";
     document.getElementById("currSong").innerHTML = gotData.song;
     document.getElementById("currArt").innerHTML = img_art;
-    console.log("Now: "+ gotData.song,gotData.artwork);
+    document.title = gotData.song;
+    //console.log("Now: "+ gotData.song,gotData.artwork);
 }
 
 async function get_url(my_url){
