8750c6a1a35826475676f8a08ecf541a18e73166
Diego Lopez <ndzerglink@live.jp>
2022-09-14 11:54:14 +0900

    Added uptime var

--- thirdRock@moji.physics/extension.js
+++ thirdRock@moji.physics/extension.js
@@ -16,6 +16,7 @@ let _httpSession;
 let _currSong;
 let sourceId = null;
 const myUrl="https://feed.tunein.com/profiles/s151799/nowPlaying";
+const upTime = 238; // seconds
 
 function init(){
 }
@@ -27,7 +28,7 @@ function enable(){
 
     get_json_async();
     Main.panel._rightBox.insert_child_at_index(panelBtn,0);
-    sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT,237,()=>{
+    sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT,upTime,()=>{
 	get_json_async();
 	return GLib.SOURCE_CONTINUE;
     });
@@ -76,7 +77,7 @@ function get_json_async(){
 	    _currSong = gotData["Header"]["Subtitle"];
 	    /*Manage string here, split by '-' if song is too long*/
 	    panelBtnLbl = new St.Label({
-		text: String.fromCharCode(9835) + "3rdRock",
+		text: String.fromCharCode(9835) + heure + " thirdRock",
 		y_align: Clutter.ActorAlign.CENTER,
 	    });
 	    panelBtn.set_child(panelBtnLbl);
