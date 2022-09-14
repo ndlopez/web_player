807459e51e00e186651ce5e608bfc0d8f74ec990
Diego Lopez <ndzerglink@live.jp>
2022-09-14 14:18:07 +0900

    Title on playlist fixed

--- html_display/get_fmLaPaz.js
+++ html_display/get_fmLaPaz.js
@@ -27,7 +27,7 @@ setInterval(async function buildList(){
     artwork = artwork[0].innerHTML.split('"');
     //songs.push(playTime+" "+song[1].innerHTML);
     //artUrls.push(artwork[1]);
-    var thisSong = song[1].innerHTML; // .split("-");
+    //var thisSong = song[1].innerHTML; // .split("-");
     const divList = document.getElementById("playList");
     const mainDiv = document.createElement("div");
     mainDiv.setAttribute("class","row");
@@ -37,7 +37,7 @@ setInterval(async function buildList(){
     divColImg.innerHTML = "<img src='"+ artwork[1]+"' width='75'>";
     var divText = document.createElement("div");
     divText.setAttribute("class","colArtist");
-    divText.innerHTML = "<span>" + thisSong[0] + "<br/><br/>" + thisSong[1] +"</span>";
+    divText.innerHTML = "<span>" + song[1].innerHTML + "<br/><br/>" + song[2].innerHTML +"</span>";
     var divTime = document.createElement("div");
     divTime.setAttribute("class","colTime");
     divTime.innerHTML = "<span>" + playTime[0] + "</span>";
