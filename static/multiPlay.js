8e7eb6608f6d7c1d33da217538cf904a0dfb5aec
Diego Lopez <ndzerglink@live.jp>
2023-04-21 21:25:42 +0900

    Re-designed index UI

--- static/multiPlay.js
+++ static/multiPlay.js
@@ -4,8 +4,6 @@
     const stream_url = "https://rfcmedia3.streamguys1.com/thirdrock.mp3";
     alt-x logo: "https://static.wixstatic.com/media/143966_f7c1536f838a4adb890693dcdbf8423f~mv2.jpg/v1/fill/w_498,h_491,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/113fm_alt_x_1001.jpg" 
 
-    id3_info: https://player.181fm.com/streamdata.php?h=listen.181fm.com&p=7080&i=181-classical_128k.mp3&https=&f=ice&c=818600
-    stream_url: https://listen.181fm.com/181-classical_128k.mp3?listenerId=esAdblock0185051&aw_0_1st.playerid=esPlayer&aw_0_1st.skey=1670382069
     history: https://player.181fm.com/external.php?http%3A%2F%2Flisten.181fm.com%3A8443%2Fice_history.php?h=listen.181fm.com&p=7080&i=181-90salt_128k.mp3&https=&f=ice&c=802268
     80s: "assets/181fm_logo.png"
     thebuzz_hist: https://player.181fm.com/external.php?http%3A%2F%2Flisten.181fm.com%3A8443%2Fice_history.php?h=listen.181fm.com&p=7080&i=181-buzz_128k.mp3&https=&f=ice&c=128773
@@ -115,10 +113,11 @@ function display_all_stations(){
     
     for(let idx = 0; idx < stations.length; idx++){
         const rowDiv = document.createElement("div");
-        rowDiv.setAttribute("class","row round-border dashed-border bottom-10px");
+        rowDiv.setAttribute("class","row col3 round-border dashed-border bottom-10px");
         rowDiv.setAttribute("id","station_"+idx);
         rowDiv.setAttribute("onclick","init_player("+idx+")");
-        //rowDiv.style.height = "160px";
+        rowDiv.style.float= "left";
+        rowDiv.style.height = "250px";
         /*rowDiv.style.backgroundImage = "url('" + stations[idx].logo +"')";
         rowDiv.style.backgroundRepeat = "no-repeat";
         rowDiv.style.backgroundSize = "cover";*/
